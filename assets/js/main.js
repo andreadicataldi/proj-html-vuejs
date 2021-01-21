536feebf34ee1c699714d08a938267175b672b30
Andrea Di Cataldi <andrea.dicataldi@gmail.com>
2021-01-21 11:05:54 +0100

    page html and css complete, header and footer generated via Vue JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,41 @@
 var app = new Vue({
   el: "#app",
-  data: {},
+  data: {
+    topLinks: ["HOME", "SERVICES", "SKILLS", "PARTNERS", "BLOG"],
+    colSections: [
+      {
+        title: "About",
+        links: [
+          "The Company",
+          "Institutional",
+          "Social & Events",
+          "Innovation",
+          "Environment",
+          "Technology",
+        ],
+      },
+      {
+        title: "Transport",
+        links: [
+          "Industrialized",
+          "Chemicals",
+          "Packaged Liquids",
+          "Construction",
+          "Laminated Wood",
+          "And others",
+        ],
+      },
+      {
+        title: "Support",
+        links: [
+          "Responsibility",
+          "Terms of Use",
+          "About Cookies",
+          "Privacy Policy",
+          "Accessibility",
+          "Information",
+        ],
+      },
+    ],
+  },
 });
